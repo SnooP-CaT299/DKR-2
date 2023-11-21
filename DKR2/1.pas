f560b5915cbdc9e09216966929d0af4d0e165098
SnooP-CaT299 <131278218+SnooP-CaT299@users.noreply.github.com>
2023-11-21 12:03:48 +0300

    Add files via upload

---
+++ DKR2/1.pas
@@ -0,0 +1,38 @@
+﻿program qwerty;
+
+var
+  arr: array of Integer;
+  n, i, j, min, A, B: Integer;
+
+begin
+  Write('Введите размер массива: ');
+  ReadLn(n);
+  SetLength(arr, n);
+
+  for i := 0 to n - 1 do
+  begin
+    Write('Введите элемент массива ', i+1, ': ');
+    ReadLn(arr[i]);
+  end;
+
+  min := MaxInt;
+
+  for i := 0 to n-1 do
+  begin
+    for j := i+1 to n-1 do
+    begin
+      if abs(arr[i] - arr[j]) < min then
+      begin
+        min := abs(arr[i] - arr[j]);
+        A := arr[i];
+        B := arr[j];
+      end;
+    end;
+  end;
+
+  WriteLn('Два элемента, разность между которыми минимальна:');
+  WriteLn('Элемент A: ', A);
+  WriteLn('Элемент B: ', B);
+  WriteLn('Разность по модулю: ', min);
+  ReadLn;
+end.
---
+++ DKR2/2.pas
@@ -0,0 +1,38 @@
+﻿program qwerty;
+
+var
+  str: string;
+  wordCount, charCount: integer;
+  i: integer;
+  inWord: boolean;
+
+begin
+  Write('Введите строку:');
+  ReadLn(str);
+
+  wordCount := 0;
+  charCount := 0;
+  inWord := false;
+
+  for i := 1 to Length(str) do
+  begin
+    if (str[i] <> ' ') and (str[i] <> '.') and (str[i] <> ',') then
+    begin
+      if not inWord then
+      begin
+        inWord := true;
+        wordCount := wordCount + 1;
+      end;
+      charCount := charCount + 1;
+    end
+    else
+    begin
+      inWord := false;
+    end;
+  end;
+
+  WriteLn('Количество слов в строке: ', wordCount);
+  WriteLn('Количество символов в строке: ', charCount);
+
+  readln;
+end.
